c73012740f16bd04fd01050f3458cf57fa43e9ac
Yota Toyama <raviqqe@gmail.com>
2024-08-21 19:48:48 +0900

    Fix

--- tests/cfg.rs
+++ tests/cfg.rs
@@ -1,12 +1,27 @@
 use cfg_exif::cfg;
 
 #[test]
-fn cfg() {
+fn feature() {
     assert_eq!(cfg!(if (feature == "foo") { 13 } else { 42 }), 42);
     assert_eq!(
         cfg!(if (feature == "foo") {
             13
         } else if (feature == "bar") {
+            13
+        } else {
+            42
+        }),
+        42
+    );
+}
+
+#[test]
+fn target_os() {
+    assert_eq!(cfg!(if (target_os == "windows") { 13 } else { 42 }), 42);
+    assert_eq!(
+        cfg!(if (target_os == "windows") {
+            13
+        } else if (target_os == "fuchsia") {
             13
         } else {
             42
