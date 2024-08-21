2a9bccdd10f579146c56d892167226ef2ff65f5c
Yota Toyama <raviqqe@gmail.com>
2024-08-21 20:11:29 +0900

    Fix

--- tests/cfg.rs
+++ tests/cfg.rs
@@ -2,12 +2,12 @@ use cfg_exif::cfg;
 
 #[test]
 fn feature() {
-    assert_eq!(cfg!(if (feature == "foo") { 13 } else { 42 }), 42);
+    assert_eq!(cfg!(if (feature == "foo") { 0 } else { 42 }), 42);
     assert_eq!(
         cfg!(if (feature == "foo") {
-            13
+            0
         } else if (feature == "bar") {
-            13
+            1
         } else {
             42
         }),
@@ -17,12 +17,12 @@ fn feature() {
 
 #[test]
 fn target_os() {
-    assert_eq!(cfg!(if (target_os == "windows") { 13 } else { 42 }), 42);
+    assert_eq!(cfg!(if (target_os == "windows") { 0 } else { 42 }), 42);
     assert_eq!(
         cfg!(if (target_os == "windows") {
-            13
+            0
         } else if (target_os == "fuchsia") {
-            13
+            1
         } else {
             42
         }),
@@ -32,24 +32,38 @@ fn target_os() {
 
 #[test]
 fn not() {
-    assert_eq!(cfg!(if (not(feature == "foo")) { 42 } else { 13 }), 42);
+    assert_eq!(cfg!(if (not(feature == "foo")) { 42 } else { 0 }), 42);
     assert_eq!(
         cfg!(if (not(feature == "foo")) {
             42
-        } else if (not(feature == "bar")) {
+        } else if (not(target_os == "fuchsia")) {
+            0
+        } else {
+            1
+        }),
+        42
+    );
+}
+
+#[test]
+fn mix() {
+    assert_eq!(
+        cfg!(if (feature == "foo") {
+            0
+        } else if (not(target_os == "fuchsia")) {
             42
         } else {
-            13
+            1
         }),
         42
     );
     assert_eq!(
-        cfg!(if (target_os == "fuchsia") {
-            0
-        } else if (feature == "foo") {
-            1
-        } else {
+        cfg!(if (not(target_os == "fuchsia")) {
             42
+        } else if (feature == "foo") {
+            0
+        } else {
+            1
         }),
         42
     );
