ce0350375259d5bd49c53fa7d1d2a7a9f0eb293b
Yota Toyama <raviqqe@gmail.com>
2024-08-21 23:45:38 +0900

    Fix

--- tests/feature.rs
+++ tests/feature.rs
@@ -26,3 +26,32 @@ fn feature() {
         42
     );
 }
+
+#[test]
+fn not() {
+    assert_eq!(feature!(if (!"foo") { 42 } else { 13 }), 42);
+}
+
+#[test]
+fn mix() {
+    assert_eq!(
+        feature!(if (!"foo") {
+            42
+        } else if ("bar") {
+            13
+        } else {
+            13
+        }),
+        42
+    );
+    assert_eq!(
+        feature!(if ("foo") {
+            13
+        } else if (!"bar") {
+            42
+        } else {
+            13
+        }),
+        42
+    );
+}
